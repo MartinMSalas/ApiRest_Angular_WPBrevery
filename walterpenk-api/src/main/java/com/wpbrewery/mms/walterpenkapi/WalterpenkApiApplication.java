ddbf7900d3b6e6f3db81c7722275b1a18b46b1eb
MartinMSalas <ingmartinmsalas@gmail.com>
2024-01-13 12:34:29 -0300

    adding files

---
+++ walterpenk-api/src/main/java/com/wpbrewery/mms/walterpenkapi/WalterpenkApiApplication.java
@@ -0,0 +1,13 @@
+package com.wpbrewery.mms.walterpenkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalterpenkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalterpenkApiApplication.class, args);
+	}
+
+}
---
+++ walterpenk-api/src/test/java/com/wpbrewery/mms/walterpenkapi/WalterpenkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpbrewery.mms.walterpenkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalterpenkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
