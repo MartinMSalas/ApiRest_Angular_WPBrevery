3407d7f2df421d04d1a509a5d7b665279426924f
MartinMSalas <ingmartinmsalas@gmail.com>
2024-01-16 00:15:56 -0300

    feat: JPA validation

--- walterpenk/src/test/java/com/wpbrewery/mms/walterpenk/repository/BeerRepositoryTest.java
+++ walterpenk/src/test/java/com/wpbrewery/mms/walterpenk/repository/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.wpbrewery.mms.walterpenk.repository;
 
 import com.wpbrewery.mms.walterpenk.entity.Beer;
+import com.wpbrewery.mms.walterpenk.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,8 +18,14 @@ class BeerRepositoryTest {
 
     @Test
     void testSaveBeer() {
-        Beer savedBeer = beerRepository.save(Beer.builder().beerName("My Beer").build());
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                            .beerName("My Beer")
+                            .beerStyle(BeerStyle.IPA)
+                            .upc("123456789")
+                            .price(new BigDecimal("12.99"))
+                .build());
 
+        beerRepository.flush();
         assertNotNull(savedBeer);
         assertNotNull(savedBeer.getId());
     }
