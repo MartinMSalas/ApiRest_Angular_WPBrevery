5822e4bf2a76a473c1967341324d1ae741ae3453
MartinMSalas <ingmartinmsalas@gmail.com>
2024-01-25 23:32:51 -0300

    fix: fix Customer maria error

--- walterpenk/src/main/java/com/wpbrewery/mms/walterpenk/entity/Customer.java
+++ walterpenk/src/main/java/com/wpbrewery/mms/walterpenk/entity/Customer.java
@@ -2,9 +2,13 @@ package com.wpbrewery.mms.walterpenk.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import org.hibernate.type.descriptor.sql.CharTypeDescriptor;
 
 @Getter
 @Setter
@@ -17,6 +21,7 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @JdbcType(CharTypeDescriptor.class)
     private UUID id;
     private String name;
     @Version
