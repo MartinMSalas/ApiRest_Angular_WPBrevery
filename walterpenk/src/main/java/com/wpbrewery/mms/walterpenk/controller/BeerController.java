3cb90e9ea1757b466da63cf8debb5628a4dc390e
MartinMSalas <ingmartinmsalas@gmail.com>
2024-03-05 23:52:40 -0300

    fix: BeerController

--- walterpenk/src/main/java/com/wpbrewery/mms/walterpenk/controller/BeerController.java
+++ walterpenk/src/main/java/com/wpbrewery/mms/walterpenk/controller/BeerController.java
@@ -97,55 +97,7 @@ public class BeerController {
                                    @RequestParam(required = false) Integer pageSize){
         Page<BeerDTO> beerDTOPage = beerService.listBeers(beerName, beerStyle, showInventory, pageNumber, pageSize);
         return beerDTOPage;
-/*
-        Map<UUID, BeerDTO> beerMap;
 
-
-        beerMap = new HashMap<>();
-
-        BeerDTO beer1 = BeerDTO.builder()
-                .id(UUID.randomUUID())
-                .version(1)
-                .beerName("Galaxy Cat")
-                .beerStyle(BeerStyle.PALE_ALE)
-                .upc("12356")
-                .price(new BigDecimal("12.99"))
-                .quantityOnHand(122)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
-
-            BeerDTO beer2 = BeerDTO.builder()
-                    .id(UUID.randomUUID())
-                    .version(1)
-                    .beerName("Crank")
-                    .beerStyle(BeerStyle.PALE_ALE)
-                    .upc("12356222")
-                    .price(new BigDecimal("11.99"))
-                    .quantityOnHand(392)
-                    .createdDate(LocalDateTime.now())
-                    .updateDate(LocalDateTime.now())
-                    .build();
-
-            BeerDTO beer3 = BeerDTO.builder()
-                    .id(UUID.randomUUID())
-                    .version(1)
-                    .beerName("Sunshine City")
-                    .beerStyle(BeerStyle.IPA)
-                    .upc("12356")
-                    .price(new BigDecimal("13.99"))
-                    .quantityOnHand(144)
-                    .createdDate(LocalDateTime.now())
-                    .updateDate(LocalDateTime.now())
-                    .build();
-
-            beerMap.put(beer1.getId(), beer1);
-            beerMap.put(beer2.getId(), beer2);
-            beerMap.put(beer3.getId(), beer3);
-        //return new PageImpl<>(new ArrayList<>(beerMap.values()));
-        return new PageImpl<>(new ArrayList<>());
-
- */
     }
 
 
